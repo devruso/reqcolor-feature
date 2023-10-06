663fd602aa824a3e314d15b9cc038c253b1ed7c2
devruso <jamilsonjr@live.com>
2023-10-06 01:17:15 -0300

    feat/wip: conditional bg change

--- src/components/ShowRating/index.jsx
+++ src/components/ShowRating/index.jsx
@@ -14,16 +14,59 @@ const TaskItemData = (props) => {
     const grandParent = parent.parentNode;
     const grandGrandParent = grandParent.parentNode;
     grandGrandParent.classList.add("grandGrandParent");
-    grandGrandParent.style.backgroundColor = "white";
   }
 
   useEffect(() => {  
     
     // Sets the background color of the task based on the time elapsed
 
+    // checks messages periodically
+        const checkMessageTime = async (props, grandGrandParent ,setBackgroundColor) => {
+  
+          const conversationSid = props.task.attributes.conversationSid;
+          const conversation = await props.manager.conversationsClient.getConversationBySid(conversationSid);
+          
+    
+    
+    
+          const calculateElapsedTime = (startDate, endDate) => {
+            const diff = endDate - startDate;
+            return Math.floor(diff / 60000); 
+          };
+    
+          
+          const messages = await conversation.getMessages();    
+          if (messages.length > 0) {
+            const lastMessage = messages[messages.length - 1];
+            const currentTime = new Date();
+            const messageTime = new Date(lastMessage.dateCreated);
+            const elapsedTime = calculateElapsedTime(messageTime, currentTime);
+    
+            if (props.manager.user.identity === lastMessage.author) {
+              setBackgroundColor("white");
+            } else {
+              if (elapsedTime >= 8) {
+                setBackgroundColor("red"); 
+              } else if (elapsedTime >= 4) {
+                setBackgroundColor("yellow"); 
+              } else {
+                setBackgroundColor("green"); 
+              }
+            }
+          }
+          grandGrandParent.style.backgroundColor = backgroundColor;
+    
+        };
 
-        // Checks messages each minute
+        
+        const interval = setInterval(() => {
+          checkMessageTime(props,grandGrandParent, setBackgroundColor);
+        }, 30000); // 30 sec
 
+    // Clears the interval when the component is destroyed
+    return () => {
+      clearInterval(interval);
+    };
 
   }, [props.task]);
 
