0204eeeba0294a89d040895a6c6b88d3e17f6430
devruso <jamilsonjr@live.com>
2023-10-06 18:25:34 -0300

    refact: updateBackground moved

--- src/components/ShowRating/index.jsx
+++ src/components/ShowRating/index.jsx
@@ -1,52 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { withTaskContext } from "@twilio/flex-ui";
-import { calculateElapsedTime } from "../../utils";
+import { updateBackgroundColor } from "../../utils";
 import "./index.css";
 
 const TaskItemData = (props) => {
 
   const [backgroundColor, setBackgroundColor] = useState("#e1e3ea");
   const child = document.querySelector(".timeComponentContainerSla");
+  const taskCreator = props.task._task.attributes.initiatedBy;
   let parent, grandParent, grandGrandParent;
 
   useEffect(() => {  
+    // Sets the background color of the task based on the interval
+    updateBackgroundColor(props, taskCreator , setBackgroundColor)
 
-    // Sets the background color of the task based on the time elapsed
-      const updateBackgroundColor = async () => {
+    // Checks if the backgroundColor needs to be updated periodically, it is necessary to pass arguments the necessary arguments
+    const interval = setInterval(() => {
+      updateBackgroundColor(props, taskCreator, setBackgroundColor);
+    }, 1000);      
         
-        const conversationSid = props.task.attributes.conversationSid;
-        const conversation = await props.manager.conversationsClient.getConversationBySid(conversationSid);
-        const messages = await conversation.getMessages();
-
-        // Need to check if it was initiaded by a 'customer' or 'admin'
-        const taskCreator = props.task._task.attributes.initiatedBy;
-
-          // need to check if it makes sense to use the last message or the first message
-        const lastMessage = messages.items[messages.items.length - 1];
-        const lastMessageTime = new Date(lastMessage.timestamp);
-        const elapsedTime = calculateElapsedTime(lastMessageTime);
-
-          if (props.manager.user.identity === lastMessage.author || taskCreator != 'customer') {
-            setBackgroundColor("#e1e3ea");
-          } else {
-            if (elapsedTime >= 8) {
-              setBackgroundColor("#ee7f83"); 
-            } else if (elapsedTime >= 4) {
-              setBackgroundColor("#def733");
-            } else {
-              setBackgroundColor("#0efc6e");
-            }
-          }
-      };
-
-      // Checks if the backgroundColor needs to be updated periodically
-      const interval = setInterval(updateBackgroundColor, 1000);
-
       // Clear interval on unmount
       return () => {
         clearInterval(interval);
       };
-
   }, [props.task,backgroundColor]);
 
    // Access to higher divs
@@ -55,7 +31,6 @@ const TaskItemData = (props) => {
    grandParent = parent.parentNode;
    if(parent && grandParent){
      grandGrandParent = grandParent.parentNode;
-     
      grandGrandParent.classList.add("grandGrandParent")
      grandGrandParent.style.backgroundColor = backgroundColor;
    }
@@ -64,7 +39,6 @@ const TaskItemData = (props) => {
   return (
     <div className="timeComponentContainerSla" >
       <div className="timerSla">
-        
       </div>
     </div>
   );
--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,25 @@ const calculateElapsedTime = (startDate) => {
     return diffInMinutes;
   };
 
+  export async function updateBackgroundColor(props,taskCreator, setBackgroundColor){
+    const conversationSid = props.task.attributes.conversationSid;
+    const conversation = await props.manager.conversationsClient.getConversationBySid(conversationSid);
+    const messages = await conversation.getMessages();
+    const lastMessage = messages.items[messages.items.length - 1];
+    const lastMessageTime = new Date(lastMessage.timestamp);
+    const elapsedTime = calculateElapsedTime(lastMessageTime);
+
+      if (props.manager.user.identity === lastMessage.author || taskCreator != 'customer') {
+        setBackgroundColor("#e1e3ea");
+      } else {
+        if (elapsedTime >= 8) {
+          setBackgroundColor("#ee7f83"); 
+        } else if (elapsedTime >= 4) {
+          setBackgroundColor("#def733");
+        } else {
+          setBackgroundColor("#0efc6e");
+        }
+      }
+  };
 
 
-export { calculateElapsedTime };
