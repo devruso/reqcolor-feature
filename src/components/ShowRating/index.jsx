688edb0e859195cbba71b57c17a9bac722292da4
devruso <jamilsonjr@live.com>
2023-10-06 01:14:23 -0300

    feat: access to grand grand parent

--- src/components/ShowRating/index.jsx
+++ src/components/ShowRating/index.jsx
@@ -1,29 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { withTaskContext } from "@twilio/flex-ui";
+import { calculateElapsedTime, checkMessageTime } from "../../utils";
+import moment from "moment";
 import "./index.css";
 
 const TaskItemData = (props) => {
+  const [backgroundColor, setBackgroundColor] = useState("white");
+  
+  // Access to higher divs
+  const child = document.querySelector(".timeComponentContainerSla");
+  if(child){
+    parent = child.parentNode;
+    const grandParent = parent.parentNode;
+    const grandGrandParent = grandParent.parentNode;
+    grandGrandParent.classList.add("grandGrandParent");
+    grandGrandParent.style.backgroundColor = "white";
+  }
 
-  useEffect(async() => {
-    let conversationSid = props.task.attributes.conversationSid
-    let conversation =  await props.manager.conversationsClient.getConversationBySid(conversationSid)
-    let {items} = await conversation.getMessages()
-
-    let message = items.find(e => e.index === conversation.lastMessage.index)
-
-    if(props.manager.user.identity === message.author){ // it was the attendant who sent the last message
-      // CHANGE TO DEFAULT COLOR
-      
-    }else{// it was the customer
-      // outside the TIME TO RESPONSE THE CUSTOMER
-    }
+  useEffect(() => {  
     
+    // Sets the background color of the task based on the time elapsed
+
+
+        // Checks messages each minute
+
+
   }, [props.task]);
 
   return (
-    <div className="timeComponentContainerSla">
+    <div className="timeComponentContainerSla" >
       <div className="timerSla">
-        <h1>Olaaaa</h1>
+        
       </div>
     </div>
   );
